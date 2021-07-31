8e24bea467e4cef697811df5996251973270e0a6
Liao CY <liaochungyid@gmail.com>
2021-07-31 15:36:57 +0800

    finish search bar rearrange varibales and add some commet on app.js pass keyword on search bar to index page, in order to keep keyword in the input box (value = "keyword")

--- app.js
+++ app.js
@@ -5,23 +5,36 @@ const port = 3000
 
 // require express-handlebars
 const exphbs = require('express-handlebars')
-const restaurantList = require("./restaurant.json")
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// require restaurants data
+const restaurantList = require("./restaurant.json")
+
 // setting static files
 app.use(express.static('public'))
 
 // routes setting
+// index page
 app.get('/', (req, res) => {
   res.render('index', { item: restaurantList.results })
 })
 
+// show page
 app.get('/restaurants/:id', (req, res) => {
   const restaurant = restaurantList.results.filter(restaurant => restaurant.id = Number(req.params.id))
   res.render('show', { item: restaurant[0] })
+})
+
+// search (index page)
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
+  const restaurants = restaurantList.results.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+  )
+  res.render('index', { item: restaurants, keyword })
 })
 
 // start and listen on the Express server
