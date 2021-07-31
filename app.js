84921b63f898188d90f4208222f20e7e7b6f1b32
Liao CY <liaochungyid@gmail.com>
2021-07-31 13:31:38 +0800

    init template for restaurant_list, include express module, bootstrap, express-handlebars, main template, index partial template, show partial template and downloaded restaurant.json from AC

---
+++ app.js
@@ -0,0 +1,25 @@
+// require express
+const express = require("express")
+const app = express()
+const port = 3000
+
+// require express-handlebars
+const exphbs = require('express-handlebars')
+const restaurantList = require("./restaurant.json")
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render("index")
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
